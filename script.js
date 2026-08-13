4a34d6c84d3098f97fd9b2e9f4537c5f6fdc1af2
bruno valente <bruno.valente.andrade@escola.pr.gov.br>
2026-08-13 10:01:52 -0300

    2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const avança = document.querySelectorAll(". btn-proximo")
+const avanca = document.querySelectorAll('.btn-proximo');
 
-avança.forEach(button => {
+avanca.forEach(button => {
     button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo')
+        const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
-        document.getElementById(proximoPasso). classList.add('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
